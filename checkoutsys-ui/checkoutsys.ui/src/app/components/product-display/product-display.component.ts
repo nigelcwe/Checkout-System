882e5a5b5cda93507871cbdbb51107a26e5a1d45
Nigel Chan <chanwenigel@gmail.com>
2023-01-18 16:04:44 +0800

    Added form logic to product display

--- checkoutsys-ui/checkoutsys.ui/src/app/components/product-display/product-display.component.ts
+++ checkoutsys-ui/checkoutsys.ui/src/app/components/product-display/product-display.component.ts
@@ -5,6 +5,7 @@ import { Component, OnInit } from '@angular/core';
 import { Router, ActivatedRoute } from '@angular/router';
 import { Product } from 'src/app/models/product';
 import { User } from 'src/app/models/user';
+import { FormControl, FormGroup, FormBuilder, Validators } from '@angular/forms';
 
 declare var window: any;
 
@@ -19,14 +20,20 @@ export class ProductDisplayComponent implements OnInit {
   currProduct!: Product;
   formModal: any;
   loading = false;
+  submitted = false;
 
   constructor(
     private userService: UserService,
     private productService: ProductService,
     private router: Router,
     private route: ActivatedRoute,
-    
+    private formBuilder: FormBuilder,
+
   ) { }
+
+  form: FormGroup = new FormGroup({
+    qty: new FormControl('')
+  })
 
   ngOnInit(): void {
     this.subscription.add(this.userService.currUser$.subscribe(user => {
@@ -36,11 +43,21 @@ export class ProductDisplayComponent implements OnInit {
       this.currProduct = product;
     }))
 
+    this.form = this.formBuilder.group({
+      qty: ['', [Validators.required, Validators.min(0)]]
+    })
+
     this.formModal = new window.bootstrap.Modal(
       document.getElementById("productModal")
     );
 
     this.formModal.show();
+  }
+
+  get f() { return this.form.controls; }
+
+  onSubmit() {
+
   }
 
 }
